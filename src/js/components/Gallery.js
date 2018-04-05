653df5d90ad310e05f606c0de2be9695f3f69c3c
Daniel Szaniszlo <daniel.szaniszlo@ericsson.com>
2018-04-05 15:52:40 -0700

    Gallery improvements

--- src/js/components/Gallery.js
+++ src/js/components/Gallery.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
+import "../../style/galleryStyle.less";
 
 class Gallery extends Component {
     constructor(props) {
@@ -7,17 +8,23 @@ class Gallery extends Component {
         this.images = props.content.images;
         this.state = {backgroundIndex: 0};
         this.animDuration = props.content.animDuration;
-        this.changeBackground = this.changeBackground.bind(this);
         this.headline = props.content.coverHeadline;
         this.text = props.content.coverText;
+        
+        this.changeBackground = this.changeBackground.bind(this);
+        this.handleNextClick = this.showNext.bind(this);
+        this.handlePrevClick = this.showPrev.bind(this);
     }
 
     componentDidMount() {
+        this.moveProgress();
         this.timeout = setTimeout(this.changeBackground, this.animDuration);
     }
 
     componentWillUnmount() {
         if (this.timeout) clearTimeout(this.timeout);
+        if (this.progreesBarIntervalId)
+            clearTimeout(this.progreesBarIntervalId);
     }
 
     changeBackground() {
@@ -34,6 +41,42 @@ class Gallery extends Component {
                 );
             }
         );
+        this.moveProgress();
+    }
+
+    showNext() {
+        this.setState(function({backgroundIndex}) {
+            let nextBackgroundIndex = ++backgroundIndex % this.images.length;
+            return {backgroundIndex: nextBackgroundIndex};
+        });
+    }
+
+    showPrev() {
+        this.setState(function({backgroundIndex}) {
+            let nextBackgroundIndex = Math.abs(
+                --backgroundIndex % this.images.length
+            );
+            return {
+                backgroundIndex: nextBackgroundIndex
+            };
+        });
+    }
+
+    moveProgress() {
+        let elem = document.querySelector(".imgContainer-progress-bar");
+        let width = 0;
+        this.progreesBarIntervalId = setInterval(
+            frame,
+            this.animDuration / 100
+        );
+        function frame() {
+            if (width >= 100) {
+                clearInterval(this.progreesBarIntervalId);
+            } else {
+                width++;
+                elem.style.width = width + "%";
+            }
+        }
     }
 
     render() {
@@ -49,6 +92,19 @@ class Gallery extends Component {
                         <p className="align-middle text-justify font-weight-light lead">
                             {this.text}
                         </p>
+                    </div>
+                    <div className="imgContainer-arrowContainer">
+                        <div
+                            className="imgContainer-arrowContainer-arrowLeft"
+                            onClick={this.handlePrevClick}
+                        />
+                        <div
+                            className="imgContainer-arrowContainer-arrowRight"
+                            onClick={this.handleNextClick}
+                        />
+                    </div>
+                    <div className="imgContainer-progress">
+                        <div className="imgContainer-progress-bar" />
                     </div>
                 </div>
             </div>
